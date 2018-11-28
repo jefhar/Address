c55d76210ae6d9a09e001b91b6282cf70a48b9d6
Jeff Harris <jeff@jeffharris.us>
2018-11-27 17:21:30 -0800

    Initial Organization.

---
+++ src/StreetAddress/StreetPrefix.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * PHP Version 7.2
+ *
+ * Address
+ */
+
+declare(strict_types=1);
+
+namespace JefHar\Address\StreetAddress;
+
+class StreetPrefix
+{
+
+}
---
+++ tests/ZipCodeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JefHar\Tests;
+
+use JefHar\Address\ZipCode;
+use PHPUnit\Framework\TestCase;
+
+class ZipCodeTest extends TestCase
+{
+
+}
