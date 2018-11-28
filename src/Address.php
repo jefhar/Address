8672b1d8356bd01e2ed8075122146fd24fe56e19
Jeff Harris <jeff@jeffharris.us>
2018-11-27 19:21:05 -0800

    Add ZipCode to Address.

--- src/Address.php
+++ src/Address.php
@@ -11,5 +11,31 @@ namespace JefHar\Address;
 
 class Address
 {
+    /** @var ZipCode */
+    private $ZipCode;
 
+    /**
+     * @param ZipCode $ZipCode
+     */
+    public function addZipCode(ZipCode $ZipCode)
+    {
+        $this->ZipCode = $ZipCode;
+    }
+
+    public function getZipCode(): ZipCode
+    {
+        return $this->ZipCode;
+    }
+
+    /**
+     * @return string
+     * @throws Exception\InvalidZipCode
+     */
+    public function getFullZip(): string
+    {
+        if (is_null($this->ZipCode)) {
+            $this->ZipCode = new ZipCode();
+        }
+        return $this->ZipCode->getFullZip();
+    }
 }
--- tests/AddressTest.php
+++ tests/AddressTest.php
@@ -3,6 +3,7 @@
 namespace JefHar\Tests;
 
 use JefHar\Address\Address;
+use JefHar\Address\ZipCode;
 use PHPUnit\Framework\TestCase;
 
 class AddressTest extends TestCase
@@ -13,6 +14,26 @@ class AddressTest extends TestCase
     public function canInstantiateAddressClass()
     {
         $address = new Address();
-        $this->assertInstanceOf(\JefHar\Address\Address::class, $address);
+        $this->assertInstanceOf(Address::class, $address);
+    }
+
+    /**
+     * @test
+     * @throws \JefHar\Address\Exception\InvalidZipCode
+     */
+    public function addressCanAddZipCode()
+    {
+        $Address = new Address();
+        $ZipCode = new ZipCode();
+        $Address->addZipCode($ZipCode);
+        $this->assertInstanceOf(ZipCode::class, $Address->getZipCode());
+        $this->assertEquals('000000000', $Address->getZipCode()->getZip9());
+        $this->assertEquals('00000-0000', $Address->getFullZip());
+
+        $ZipCode->setZip5('123451234');
+        $this->assertEquals('12345-1234', $Address->getFullZip());
+
+        $Address = new Address();
+        $this->assertEquals('00000-0000', $Address->getFullZip());
     }
 }
